0fb0c5607c77429577ee873f4be5402cdfce94f7
Olimpia Spyra <olimpia.spyra@gmail.com>
2023-01-03 16:21:07 +0100

    Add basic styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import '../src/styles/normalize.scss';
+import '../src/styles/global.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
